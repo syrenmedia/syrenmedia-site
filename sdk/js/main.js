11f436f2b2a2dcec5f78e4a8d26290c30a20be46
Steven Kornblum <sdk1221@gmail.com>
2016-01-31 11:36:00 -0800

    cleanup

--- sdk/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $(".fa").click(function() {
-    $(this).parent().parent().find("ul").toggle("slow");
-    $(this).parent().parent().find("hr").toggle("slow");
-    $(this).toggleClass("fa-angle-double-down");
-  });
-});
-
